9bb9086b09348d9a4c8acd9f5765d52db494d0c1
Agustin B <agustin.burset@icloud.com>
2018-02-04 18:04:12 -0300

    configuracion inicial de webpack

---
+++ src/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="app">
+		<h1> {{ message }} </h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'app',
+		data () {
+			return {
+				message: 'Learning Webpack !!'
+			}
+		}
+	}
+</script>
+
+<style>
+	#app {
+		font-family: sans-serif;
+	}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+var webpack = require('webpack');
+var path = require('path');
+var inProduction = (process.env.NODE_ENV === 'production');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	entry: {
+		app: ['./src/js/main.js'],
+		vendor: ['vue']
+		// screen: [...]
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{
+				// loader para uso de Vue Single-file-components (.vue)
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					loaders: {
+						// extraigo el estilo de los componentes a un archivo aparte
+						// (para referencia desde las vistas)
+						css: ExtractTextPlugin.extract({
+							use: 'css-loader'
+			          	})
+					}
+				}
+			},
+			{ 
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		alias: {
+	    	'vue$': 'vue/dist/vue.esm.js'
+	  	}
+	},
+	plugins: [
+		// junto todas las librerias en uso en un archivo aparte
+		// (en app.js queda solo el codigo de la applicacion)
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor'
+		}),
+		// extraccion de estilos a archivo aparte
+		new ExtractTextPlugin("[name].css")
+	]
+};
+
+if (inProduction) {
+	module.exports.plugins.push(
+		new webpack.optimize.UglifyJsPlugin()
+	);
+	// algunos plugins usan LoaderOptionsPlugin para determinar su salida (css minificado o no, etc)
+	module.exports.plugins.push(
+		new webpack.LoaderOptionsPlugin({
+			minimize: true
+		})
+	);
+}
