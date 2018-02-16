d60fc14be0c03bc704998215d639e70d3774326e
Agustin Burset <agustin.burset@icloud.com>
2018-02-16 04:22:56 -0300

    limpieza de codigo y archivos sin uso

--- application/controllers/Api_old.php
+++ application/controllers/Api_old.php
@@ -10,254 +10,31 @@ class Api_old extends REST_Controller {
 		parent::__construct();
 		// Load directory model
 		$this -> load -> model('Signage_model','model');
-		// Configure limits on our controller methods
-		// Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
-		// $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
-		// $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
-		// $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
 	}
-
-	// GRUPOS
-
-		public function group_get() {
-			$group = NULL;
-			$id = $this -> get('id');
-			$includeContent = filter_var ($this -> get('includeContent'), FILTER_VALIDATE_BOOLEAN);
-			$includePath = filter_var ($this -> get('includePath'), FILTER_VALIDATE_BOOLEAN);
-
-			// si se paso un id vacio devuelvo el grupo raiz
-			if ($id === '') {
-				$group = $this -> model -> getGroup(NULL, $includeContent, $includePath);
-				$this -> response($group, REST_Controller :: HTTP_OK);
-			}
-
-			// chequeo validez del grupo
-			$this -> _isValidItem(GROUP, $id);
-
-			// devuelvo el grupo correspondiente
-			$group = $this -> model -> getGroup($id, $includeContent, $includePath);
-			$this -> response($group, REST_Controller :: HTTP_OK);
-		}
-
-		public function group_put() {
-			$name = $this -> put('name');
-			$parentId = $this -> put('parentId');
-			$extraFields = $this -> put('extraFields');
-			$this -> _addItem(GROUP, $name, $parentId, $extraFields);
-		}
-
-		public function group_post() {
-			$id = $this -> post('id');
-			$name = $this -> post('name');
-			// usar para implementar "mover"
-			// $parentId = $this -> post('parentId');
-			$extraFields = $this -> post('extraFields');
-			$this -> _updateItem(GROUP, $id, $name, $extraFields);
-		}
-
-		public function group_delete() {
-			$id = $this -> query('id');
-			$this -> _deleteItem(GROUP, $id);
-		}
-
-	// PANTALLAS
-
-		public function screen_get() {
-			$udid = $this -> get('udid');
-			$id = $this -> get('id');
-			$includeContent = filter_var($this -> get('includeContent'), FILTER_VALIDATE_BOOLEAN);
-			$screen = NULL;
-			// busqueda por udid
-			if ($udid != NULL) {
-				$where = ['udid' => $udid];
-				$screen = $this -> model -> getScreen($where, $includeContent);
-			}
-			// busqueda por id
-			if ($id != NULL) {
-				$where = ['id' => $id];
-				$screen = $this -> model -> getScreen($where, $includeContent);
-			}
-
-			if (!empty($screen)) {
-				$this -> response($screen, REST_Controller :: HTTP_OK);
-			} else {
-				$message = [
-				'message' => 'Screen not found'
-				];
-				$this -> response($message, REST_Controller :: HTTP_NOT_FOUND); 
-			}	
-		}
-
-		public function screen_put() {
-			$name = $this -> put('name');
-			$parentId = $this -> put('parentId');
-			$extraFields = $this -> put('extraFields');
-			$this -> _addItem(SCREEN, $name, $parentId, $extraFields);
-		}
-
-		public function screen_post() {
-			$id = $this -> post('id');
-			$name = $this -> post('name');
-			// usar para implementar "mover"
-			// $parentId = $this -> post('parentId');
-			$extraFields = $this -> post('extraFields');
-			$this -> _updateItem(SCREEN, $id, $name, $extraFields);
-		}
-
-		public function screen_delete() {
-			$id = $this -> query('id');
-			$this -> _deleteItem(SCREEN, $id);
-		}
-
-	// PLAYLISTS
-
-		public function playlist_get() {
-			$id = $this -> get('id');
-			$includeContent = filter_var($this -> get('includeContent'), FILTER_VALIDATE_BOOLEAN);
-			// consulto al modelo
-			$playlists = $this -> model -> getPlaylists($id, $includeContent);
-			$this -> response($playlists, REST_Controller :: HTTP_OK);
-		}
 
 	// SETUP
 
-		public function setup_put() {
-			$udid = $this -> put('udid');
-			// agrego udid de pantalla a la DB con su pin correspondiente
-			$setup = $this -> model -> addSetup($udid);
+	public function setup_put() {
+		$udid = $this -> put('udid');
+		// agrego udid de pantalla a la DB con su pin correspondiente
+		$setup = $this -> model -> addSetup($udid);
+		$message = [
+			'setup' => $setup,
+			'message' => 'Screen ready for setup'
+		];
+		$this -> response($message, REST_Controller :: HTTP_CREATED);
+	}
+
+	public function setup_get() {
+		$pin = $this -> get('pin');
+		// consulto al modelo
+		$setup = $this -> model -> getSetup($pin);
+		if (empty($setup)) {
 			$message = [
-				'setup' => $setup,
-				'message' => 'Screen ready for setup'
+				'message' => 'No screen found with pin ' . $pin
 			];
-			$this -> response($message, REST_Controller :: HTTP_CREATED);
+			$this -> response($message, REST_Controller :: HTTP_NOT_FOUND); 
 		}
-
-		public function setup_get() {
-			$pin = $this -> get('pin');
-			// consulto al modelo
-			$setup = $this -> model -> getSetup($pin);
-			if (empty($setup)) {
-				$message = [
-					'message' => 'No screen found with pin ' . $pin
-				];
-				$this -> response($message, REST_Controller :: HTTP_NOT_FOUND); 
-			}
-			$this -> response($setup, REST_Controller :: HTTP_OK);
-		}
-
-	// METODOS INTERNOS
-
-		private function _addItem($type, $name, $parentId, $extraFields) {
-			// chequeo validez del grupo destino
-			$this -> _isValidItem(GROUP, $parentId);
-
-			// chequeo validez del nombre
-			$this -> _isValidNewItemName($type, $name, $parentId);
-
-			// trato de agregar el nuevo item
-			$newItemId = $this -> model -> addItem($type, $name, $parentId, $extraFields);
-			if ($newItemId != NULL) {
-				// item agregado con exito
-				$item = $type === GROUP ? $this -> model -> getGroup($newItemId) : $this -> model -> getScreen(['id' => $newItemId]);
-				$message = [
-					'item' => $item,
-					'message' => ucfirst($type) . ' created'
-				];
-				$this -> response($message, REST_Controller :: HTTP_CREATED);
-			} else {
-				// no se pudo agregar el item
-				$message = [
-					'message' => 'Could not create ' . $type
-				];
-				$this -> response($message, REST_Controller :: HTTP_INTERNAL_SERVER_ERROR);
-			}
-		}
-
-		private function _updateItem($type, $id, $name, $extraFields) {
-			// chequeo validez del item
-			$this -> _isValidItem($type, $id);
-
-			// chequeo si se esta tratando de mover y el grupo destino es valido 
-			// ... para despues
-
-			// chequeo si se esta renombrando y su validez
-			if ($name != NULL) {
-				$this -> _isValidRename($type, $name, $id);
-			}
-
-			// trato de modificar el item correspondiente
-			if ($this -> model -> updateItem($type, $id, $name, $extraFields)) {
-				// item modificado con exito
-				$item = $type === GROUP ? $this -> model -> getGroup($id) : $this -> model -> getScreen(['id' => $id]);
-				$message = [
-					'item' => $item,
-					'message' => ucfirst($type) . ' updated'
-				];
-				$this -> response($message, REST_Controller :: HTTP_OK);
-			} else {
-				// no se pudo modificar el item
-				$message = [
-					'id' => $id,
-					'message' => 'Could not update ' . $type
-				];
-				$this -> response($message, REST_Controller :: HTTP_INTERNAL_SERVER_ERROR);
-			}
-		}
-
-		private function _deleteItem($type, $id) {
-			// chequeo validez del item
-			$this -> _isValidItem($type, $id);
-
-			// elimino el item correspondiente        
-			if ($this -> model -> deleteItem($id)) {
-				// item eliminado existosamente
-				$this -> response(NULL, REST_Controller :: HTTP_NO_CONTENT);
-			} else {
-				// no se pudo eliminar el item
-				$message = [
-					'id' => $id,
-					'message' => 'Could not delete ' . $type
-				];
-				$this -> response($message, REST_Controller :: HTTP_INTERNAL_SERVER_ERROR);
-			}
-		}
-
-		// Chequeos (para dar una respuesta apropiada a cada request)
-
-		private function _isValidItem($type, $id) {
-			// chequeo validez del id
-			if (!$this -> model -> isValidItemId($id)) {
-				$this -> response(NULL, REST_Controller :: HTTP_BAD_REQUEST);
-			}
-
-			// chequeo que el item exista      
-			if (!$this -> model -> itemExists($type, $id)) {
-				$message = [
-					'id' => $id,
-					'message' => ucfirst($type) . ' not found'
-				];
-				$this -> response($message, REST_Controller :: HTTP_NOT_FOUND);
-			}
-		}
-
-		private function _isValidNewItemName($type, $name, $parentId) {
-			if (!$this -> model -> isValidNewItemName($type, $name, $parentId)) {
-				// nombre en uso
-				$message = [
-					'message' => 'Name already in use'
-				];
-				$this -> response($message, REST_Controller :: HTTP_CONFLICT);
-			}
-		}
-
-		private function _isValidRename($type, $name, $id) {
-			if (!$this -> model -> isValidRename($type, $name, $id)) {
-				// nombre en uso
-				$message = [
-					'message' => 'Name already in use'
-				];
-				$this -> response($message, REST_Controller :: HTTP_CONFLICT);
-			}
-		}
-
+		$this -> response($setup, REST_Controller :: HTTP_OK);
+	}
 }
--- application/models/Signage_model.php
+++ application/models/Signage_model.php
@@ -7,242 +7,42 @@ class Signage_model extends CI_Model {
 		parent::__construct();
 		// cargo base de datos
 		$this -> load -> database();
-		// cargo y configuro libreria para el manejo del directorio (grupos y pantallas)
-		$this -> load -> library('nested_set', NULL, 'directory');
-		$this -> directory -> setControlParams('directory', 'left_value', 'right_value', 'id', 'parent_id', 'name');
-		// creo nuevo directorio (si no hay uno)
-		if (count($this -> directory -> getRootNodes()) == 0) {
-			$this -> _setupDirectory();
-		}
 	}
 
-	// GRUPOS Y PANTALLAS
+	// SETUP
 
-		public function getGroup($id = NULL, $includeContent = FALSE, $includePath = FALSE) {
-			$groupId = $id;
-			if ($id === NULL) {
-				$rootGroup = $this -> directory -> getRootNodes()[0];
-				$groupId = $rootGroup['id'];
-			}
-			$query = $this -> db
-			-> select('*')
-			-> from('groups')
-			-> where('id', $groupId)
-			-> get();
-			$group = $query -> row_array();
-
-			if (!empty($group)) {
-				if ($includePath) {
-					$group['path'] = $this -> _getPathData($groupId);
-				}
-				if ($includeContent) {
-					$group['content'] = $this -> _getContentData($groupId);
-				}
-			}
-
-			return $group;
-		}
-
-		public function getScreen($where, $includeContent = FALSE) {
-			$query = $this -> db
-			-> select('*')
-			-> from('screens')
-			-> where($where)
-			-> get();
-			$screen = $query -> row_array();
-
-			if (!empty($screen) && $includeContent) {
-				$screen['content'] = $this -> _getPlaylistContent($screen['playlist_id']);
-			}
-			return $screen;
-		}
-
-		public function addItem($type, $name, $parentId, $extraFields) {
-			// obtengo grupo destino
-			$parenGroup = $this -> directory -> getNodeFromId($parentId);
-			// creo nuevo item en directorio
-			$newItem = $this -> directory -> appendNewChild($parenGroup, array("name" => $name, "type" => $type));
-			// adjunto id generado a la info especifica al tipo [group/screen]
-			$extraFields['id'] = $newItem['id'];
-			// tabla destino segun el tipo (si fueran mas tipos se podria armar una loockup table)
-			$type_table = $type === GROUP ? 'groups_data' : 'screens_data';
-			// trato de insertar item
-			if ($this -> db -> insert($type_table, $extraFields)) {
-				// el item se creo con exito
-				return $newItem['id'];
-			} else {
-				// rollback del insert en el directorio (si hubo un error al insertar en la tabla con info especifica al tipo)
-				$this -> directory -> deleteNode($newItem);
-				return NULL;
-			}
-		}
-
-		public function updateItem($type, $id, $name, $extraFields) {
-			$this -> db -> trans_start();
-			// OJO! si se implementa el "mover" la transaccion no 
-			// va a andar (tiene que ver con estar usando una libreria en el medio)
-
-			// modifico tabla con datos comun a todos los tipos
-			if (!empty($name)) {
-				$this -> db -> update('directory', array('name' => $name), array('id' => $id));
-			}
-			// modifico tabla destino dependiente del tipo (si fueran mas tipos se podria armar una loockup table)
-			if (!empty($extraFields)) {
-				$type_table = $type === GROUP ? 'groups_data' : 'screens_data';
-				$this -> db -> update($type_table, $extraFields, array('id' => $id));
-			}
-			return $this -> db -> trans_complete();
-		}
-
-		public function deleteItem($id) {
-			$item = $this -> directory -> getNodeFromId($id);
-			return $this -> directory -> deleteNode($item);
-		}
-
-	// CHEQUEOS
-
-		public function isValidItemId($id) {
-			$id = (int) $id;
-			return $id > 0;
-		}
-
-		public function itemExists($type, $id) {
-			$item = $this -> directory -> getNodeFromId($id);
-			return (!empty($item) && $item['type'] === $type);
-		}
-
-		public function isValidNewItemName($type, $name, $parentId) {
-			$query = $this -> db 
-				-> select('*')
-				-> from('directory')
-				-> where(['type' => $type, 'name' => $name, 'parent_id' => $parentId])
-				-> get();
-			$result = $query -> row();
-			return empty($result);
-		}
-
-		public function isValidRename($type, $name, $id) {
-			$parentId = $this -> directory -> getNodeFromId($id)['parent_id'];
-			$query = $this -> db 
-				-> select('*')
-				-> from('directory')
-				-> where(['type' => $type, 'name' => $name, 'parent_id' => $parentId, 'id !=' => $id])
-				-> get();
-			$result = $query -> row();
-			return empty($result);
-		}
-
-	// PLAYLISTS
-
-		public function getPlaylists($id = NULL, $includeContent = NULL) {
-			// obtengo playlist/s
-			if ($id === NULL) {
-				$query = $this -> db -> get('playlists');
-				$playlists = $query -> result_array();
-				if ($includeContent) {
-					foreach ($playlists as &$playlist) {
-						$content = $this -> _getPlaylistContent($playlist['id']);
-						$playlist['items'] = $content;
-					}
-				}
-				return $playlists;
-			} else {
-				$query = $this -> db -> get_where('playlists', ['id' => $id]);
-				$playlist = $query -> row_array(); 
-				if ($includeContent) {
-					$playlist['items'] = $this -> _getPlaylistContent($playlist['id']);
-				}
-				return $playlist;
-			}
-		}
-
-		private function _getPlaylistContent($id) {
-			$query = $this -> db 
-			-> select('playlists_images.position, images.id, images.location')
-			-> from('playlists_images')
-			-> join('images', 'playlists_images.image_id = images.id')
-			-> where('playlists_images.playlist_id', $id)
-			-> order_by('position', 'ASC')
-			-> get();
-			return $query -> result_array();
-		}
-
-		public function addSetup($udid) {
-			$setup = null;
-			// chequeo si ya esta en la lista de pantallas listas para configurarse (i.e. con su pin correspondiente)
-			$query = $this -> db -> get_where('screens_setup', ['udid' => $udid]);
-			$setup = $query -> row_array();
-			// si no esta en la lista, le genero un pin y la agrego
-			if (empty($setup)) {
+	public function addSetup($udid) {
+		$setup = null;
+		// chequeo si ya esta en la lista de pantallas listas para configurarse (i.e. con su pin correspondiente)
+		$query = $this -> db -> get_where('screens_setup', ['udid' => $udid]);
+		$setup = $query -> row_array();
+		// si no esta en la lista, le genero un pin y la agrego
+		if (empty($setup)) {
+			$pin = $this -> _generatePin();
+			// chequeo que por casualidad el pin generado no este ya en uso por otra pantalla
+			$oldSetup = $this -> db -> get_where('screens_setup', ['pin' => $pin]) -> row_array();
+			while (!empty($oldSetup)) {
 				$pin = $this -> _generatePin();
-				// chequeo que por casualidad el pin generado no este ya en uso por otra pantalla
-				$oldSetup = $this -> db -> get_where('screens_setup', ['pin' => $pin]) -> row_array();
-				while (!empty($oldSetup)) {
-					$pin = $this -> _generatePin();
-				}
-				$setup = ['udid' => $udid, 'pin'=> $pin];
-				$this -> db -> insert('screens_setup', $setup);
 			}
-			return $setup;
+			$setup = ['udid' => $udid, 'pin'=> $pin];
+			$this -> db -> insert('screens_setup', $setup);
 		}
+		return $setup;
+	}
 
-		public function getSetup($pin) {
-			return $this -> db -> get_where('screens_setup', ['pin' => $pin]) -> row_array();
+	public function getSetup($pin) {
+		return $this -> db -> get_where('screens_setup', ['pin' => $pin]) -> row_array();
+	}
+
+	private function _generatePin($length = 4) {
+		$pin = "";
+		// $characters = array_merge(range('A','Z'), range('a','z'), range('0','9'));
+		$characters = array_merge(range('0','9'));
+		$max = count($characters) - 1;
+		for ($i = 0; $i < $length; $i++) {
+			$randomIndex = mt_rand(0, $max);
+			$pin .= $characters[$randomIndex];
 		}
-
-		private function _generatePin($length = 4) {
-			$pin = "";
-			// $characters = array_merge(range('A','Z'), range('a','z'), range('0','9'));
-			$characters = array_merge(range('0','9'));
-			$max = count($characters) - 1;
-			for ($i = 0; $i < $length; $i++) {
-				$randomIndex = mt_rand(0, $max);
-				$pin .= $characters[$randomIndex];
-			}
-			return $pin;
-		}
-
-	// METODOS INTERNOS / PRIVADOS
-
-		private function _getContentData($id) {
-			// obtengo grupos
-			$query = $this -> db
-				-> select('*')
-				-> from('groups')
-				-> where('parent_id', $id)
-				-> get();
-			$groups = $query -> result_array();
-			// obtengo pantallas
-			$query = $this -> db
-				-> select('*')
-				-> from('screens')
-				-> where('parent_id', $id)
-				-> get();
-			$screens = $query -> result_array();
-			// combino los arreglos
-			$contents = array_merge($groups, $screens);
-			return $contents;
-		}
-
-		private function _getPathData($id) {
-			$path = array();
-			$group = $this -> directory -> getNodeFromId($id);
-			$pathItems = $this -> directory -> getPath($group, TRUE, TRUE);
-			foreach ($pathItems as $pathItem) {
-				$path[] = array(
-					"id" => $pathItem['id'],
-					"name" => $pathItem['name']
-				);
-			};
-			return $path;
-		}
-
-		private function _setupDirectory() {
-			$this -> directory -> insertNewTree(array("name" => "Groups and Screens", "type" => GROUP));
-			$rootGroup = $this -> directory -> getRootNodes()[0];
-			// OJO! editar al modificar groups_data
-			$extraFields = [];
-			$extraFields['id'] = $rootGroup['id'];
-			$this -> db -> insert('groups_data', $extraFields);
-		}
+		return $pin;
+	}
 }
